5d0e37f700a56d49427d9baa7efc0a753a171672
Monty Panday <montypanday293@gmail.com>
2024-06-19 18:30:43 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,10 @@ ingredients_list = st.multiselect(
     my_dataframe,
     max_selections=5)
 
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response.json())
+
 if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
